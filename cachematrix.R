47b8c94a3ed221157557d06fb88d870a03df0e6f
bqmax <qmaxksmu@yandex.ru>
2018-01-28 00:53:53 +0300

    bodies of function were added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions make and cache a matrix and inverse of the matrix
 
-## Write a short comment describing this function
+# In this part of code two objects 'x' and 'm' are initialazed, 'x' will contain a matrix when makeCacheMatrix() will be runned, 's' will contain inverse of a matrix when cacheSolve() will be runned. An output object will contain
+# set the value of the matrix
+# get the value of the matrix
+# set the value of the inverse of the matrix
+# get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      s <- NULL
+      set <- function(y) {
+            x <<- y
+            s <<- NULL
+      }
+      get <- function() x
+      setsolve <- function(solve) s <<- solve
+      getsolve <- function() s
+      list(set = set, get = get,
+           setsolve = setsolve,
+           getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## In this part of code inverse of matrix will be cached
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      s <- x$getsolve()
+      if(!is.null(s)) {
+            message("getting cached data")
+            return(s)
+      }
+      data <- x$get()
+      s <- solve(data, ...)
+      x$setsolve(s)
+      s
 }
